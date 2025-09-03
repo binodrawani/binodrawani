b09b5eb584c0a0a7b37f571ea819d9296820a940
Binod Rawani <binodrawani@gmail.com>
2025-09-03 23:48:53 +0530

    fix: build error

---
+++ .next/static/chunks/app/(investment)/finance/layout-0e73c77dcaa74981.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[881],{3767:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,4680,23))},4680:()=>{}},e=>{e.O(0,[965,900,341,358],()=>e(e.s=3767)),_N_E=e.O()}]);
